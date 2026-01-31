156979dbdac34ffb5a33ed84a652abe765919a27
Dominic Larkin <dominic.larkin@westpoint.edu>
2026-01-30 22:30:53 -0500

    gazebo adjusted qos to best effort for joy and changed joy topic to cmd_vel

--- turtlebot3_manipulation_bringup/launch/gazebo.launch.py
+++ turtlebot3_manipulation_bringup/launch/gazebo.launch.py
@@ -196,7 +196,8 @@ def generate_launch_description():
             condition=IfCondition(use_joy),
             launch_arguments={'config_filepath': joy_configs,
                           'joy_dev': '0',
-                          'joy_vel': 'cmd_vel_joy',
+                        #   'joy_vel': 'cmd_vel_joy',
+                          'joy_vel': 'cmd_vel',
                           'use_sim': use_sim_time,
                           'publish_stamped_twist': 'true',}.items()),        
 
