6738247cedf7e1685018e4cb6f5d7ba51a6ff383
Dominic Larkin <dominic.larkin@westpoint.edu>
2026-03-11 12:33:31 -0400

    added ability to start rviz with moevit group2

--- turtlebot3_manipulation_moveit_config/launch/move_group2.launch.py
+++ turtlebot3_manipulation_moveit_config/launch/move_group2.launch.py
@@ -1,17 +1,33 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_move_group_launch
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
 
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import (
+    generate_move_group_launch,
+    generate_moveit_rviz_launch,
+)
 
 def generate_launch_description():
+    # Toggle to start RViz
+    use_rviz = DeclareLaunchArgument(
+        "use_rviz",
+        default_value="true",
+        choices=["true", "false"],
+        description="Start RViz with MoveIt config"
+    )
+
     moveit_config = MoveItConfigsBuilder("turtlebot3_manipulation", package_name="turtlebot3_manipulation_moveit_config").to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
 
+    ld = LaunchDescription([use_rviz])
 
-# def generate_launch_description():
-#     moveit_config = MoveItConfigsBuilder("turtlebot3_manipulation", package_name="ee484_manipulator").to_moveit_configs()
-#     log_action1 = LogInfo(msg=['MMMVVV moveit_config is: ', moveit_config])
-#     ld = generate_move_group_launch(moveit_config)
-#     log_action2 = LogInfo(msg=['MMMGGG moveit_config is: ', mg_launch])
-#     ld.add_action(log_action1)
-#     ld.add_action(log_action2)
-#     return ld
+    # MoveGroup (planning pipeline, controllers, etc.)
+    ld.add_action(generate_move_group_launch(moveit_config))
+
+    # Conditionally launch RViz with MoveIt panels and config
+    rviz_launch = generate_moveit_rviz_launch(moveit_config)
+    rviz_launch.condition = IfCondition(LaunchConfiguration("use_rviz"))
+    ld.add_action(rviz_launch)
+
+    return ld
