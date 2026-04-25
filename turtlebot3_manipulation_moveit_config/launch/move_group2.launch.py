bce31c94d4d6e03d56c45a8a28349a1bdc7073ae
Dominic Larkin <dominic.larkin@westpoint.edu>
2026-04-24 23:17:38 -0400

    enabled sim_time to be set for moveit group2

--- turtlebot3_manipulation_moveit_config/launch/move_group2.launch.py
+++ turtlebot3_manipulation_moveit_config/launch/move_group2.launch.py
@@ -5,6 +5,8 @@ from launch.substitutions import LaunchConfiguration
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir
+from launch.actions import ExecuteProcess
+
 
 
 from moveit_configs_utils import MoveItConfigsBuilder
@@ -14,30 +16,41 @@ from moveit_configs_utils.launches import (
 )
 
 def generate_launch_description():
-    # Toggle to start RViz
-
 
     use_rviz = LaunchConfiguration('use_rviz')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     moveit_config = MoveItConfigsBuilder("turtlebot3_manipulation", package_name="turtlebot3_manipulation_moveit_config").to_moveit_configs()
 
     rviz_arg = DeclareLaunchArgument(
         'use_rviz',
-        default_value='false',
+        default_value='true',
         description='Whether execute rviz2')
-    
+
+    sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Whether execute use_sim_time')    
     
     rviz_launch = IncludeLaunchDescription(
             PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/moveit_rviz2.launch.py']),
             condition=IfCondition(use_rviz),
         )
     
+    set_sim_time = ExecuteProcess(
+            cmd=['ros2', 'param', 'set', '/move_group', 'use_sim_time', 'true'],
+            output='screen',
+            condition=IfCondition(use_sim_time),
+        )
+    
     ld = LaunchDescription()
     ld.add_action(rviz_arg)  
+    ld.add_action(sim_time_arg)
 
     # MoveGroup (planning pipeline, controllers, etc.)
     ld.add_action(generate_move_group_launch(moveit_config))
     ld.add_action(rviz_launch)
+    ld.add_action(set_sim_time)
 
     # Conditionally launch RViz with MoveIt panels and config
     # rviz_launch = generate_moveit_rviz_launch(moveit_config)
